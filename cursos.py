b3bfe627a8ca69a1b4392142fc1e4ce820e7d2ab
lesepulvedav <le.sepulvedav@duocuc.cl>
2025-06-10 12:43:40 -0400

    Carga inicial del archivo
    Esta es la primera versión del archivo

---
+++ cursos.py
@@ -0,0 +1,26 @@
+cursos = []
+print("")
+contador = 0
+opcion = ""
+while opcion != "n":
+ 
+    nombre = input("Ingrese el nombre del alumno: ")
+    direccion = input("Ingrese la dirección del alumno: ")
+    telefono = input("Ingrese el teléfono del alumno: ")
+    alumno = []
+    alumno = [nombre,direccion,telefono]
+    cursos.append(alumno)
+    contador += 1
+    if contador == 30:
+        opcion = "n"
+        print("")
+    else:
+        opcion = input("Desea agregar otro alumno (s/n): ")
+        print("")
+ 
+for alumno in cursos:
+    print("Nombre: " ,alumno[0])
+    print("Dirección: ", alumno[1])
+    print("Teléfono: ", alumno[2])
+    print("") 
+
